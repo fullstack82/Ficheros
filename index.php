ca06a7d9929474541dfdbce73497b2d8472a74e4
fullstack82 <fullstack8210@gmail.com>
2021-02-02 21:21:02 +0100

    leer y escribir ficheros

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-$archivo = fopen('fichero_texto.txt', 'r');    // Abrir archivo 
+$archivo = fopen('fichero_texto.txt', 'r');    // Abrir archivo (los permisos: 'r' permite leer los archivos (a+ permite leer y escribir en los ficheros));
 
 
 // Leer contenido 
@@ -10,8 +10,6 @@ while(!feof($archivo)){
     echo $contenido."<br/>";
 
 }
-
-
 
 
 fclose($archivo);               // Cerrar archivo
